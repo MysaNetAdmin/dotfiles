b93886393261a18855e8f84f8008d8d136d0c33d
Samy Hussaein <samy.hussaein@epita.fr>
2018-09-15 21:57:43 +0200

    Added .config folder with my i3 config file

---
+++ .config/i3/i3lock.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+IMAGE=/tmp/i3lock.png
+SCREENSHOT="scrot $IMAGE"
+#BLURTYPE="2x8" # 2.90s
+BLURTYPE="8x8" # 2.92s
+
+# Get the screenshot, add the blur and lock the screen with it
+$SCREENSHOT
+convert $IMAGE -blur $BLURTYPE $IMAGE
+i3lock -i $IMAGE -p default
