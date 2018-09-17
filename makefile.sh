8cdcf3d2633502e22687f9c9e486a94971482cb8
Samy Hussaein <samy.hussaein@epita.fr>
2018-09-17 15:39:42 +0200

    Added a shell script that automatically create a Makefile with all C files included in it

---
+++ makefile.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+filename=Makefile
+
+if [ -e $filename ] ; then
+  echo "Makefile already exists" >&2
+  exit 1
+fi
+
+echo "CC= gcc" >> $filename
+echo "CFLAGS= -Wall -Wextra -Werror -std=c99 -pedantic" >> $filename
+echo "" >> $filename
+
+SRC=""
+
+for file in *.c ; do
+  if [ -f $file ] ; then
+    SRC+="$file"
+  fi
+done
+
+echo "SRC= $SRC" >> $filename
+echo "OBJ= \${SRC:.c=.o}" >> $filename
+echo "" >> $filename
+
+echo "all: \${OBJ}" >> $filename
+echo "" >> $filename
+
+echo "clean:" >> $filename
+echo -e "\t\${RM} \${OBJ}" >> $filename
+echo "" >> $filename
+
+echo ".PHONY: all clean" >> $filename
