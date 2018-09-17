7a9a4d08540236d73a94b91cfc11900b80e289d0
Samy Hussaein <samy.hussaein@epita.fr>
2018-09-17 12:00:27 +0200

    Changed i3lock config to not show the mouse

--- .config/i3/i3lock.sh
+++ .config/i3/i3lock.sh
@@ -2,10 +2,9 @@
 
 IMAGE=/tmp/i3lock.png
 SCREENSHOT="scrot $IMAGE"
-#BLURTYPE="2x8" # 2.90s
-BLURTYPE="8x8" # 2.92s
+BLURTYPE="8x8"
 
 # Get the screenshot, add the blur and lock the screen with it
 $SCREENSHOT
 convert $IMAGE -blur $BLURTYPE $IMAGE
-i3lock -e -i $IMAGE -p default
+i3lock -i $IMAGE -e
