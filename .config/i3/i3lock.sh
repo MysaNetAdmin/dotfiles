b4e90a4277eccf4b102d2e9d76c084354a142751
Samy Hussaein <samy.hussaein@epita.fr>
2018-09-24 22:37:33 +0200

    Changed lock script to choose a random image from a folder

--- .config/i3/i3lock.sh
+++ .config/i3/i3lock.sh
@@ -2,7 +2,4 @@
 
 IMAGE="$(find $HOME/Images/Lockscreen/  | sort -u | shuf -n1)"
 
-convert -pointsize 20 -fill white \
-        -draw 'text 960,1080 "Type Password to Unlock"' "$IMAGE" "$IMAGE"
-
 i3lock -i "$IMAGE" -e
