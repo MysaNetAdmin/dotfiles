eb3f172ab758aa177407d7c6faeee7083c2eba47
Samy Hussaein <samy.hussaein@epita.fr>
2019-01-26 16:56:07 +0100

    Changed makefile script for C++

--- makefile.sh
+++ makefile.sh
@@ -7,27 +7,41 @@ if [ -e $filename ] ; then
   exit 1
 fi
 
-echo "CC= gcc" >> $filename
-echo "CFLAGS= -Wall -Wextra -Werror -std=c99 -pedantic" >> $filename
+exe=main
+
+if [ $# -eq 1 ]; then
+    exe=$1
+fi
+
+echo "CXX= g++" >> $filename
+echo "CXXFLAGS= -Wall -Wextra -Werror -pedantic -std=c++17 -fsanitize=address" >> $filename
+echo "LDLIBS= asan" >> $filename
 echo "" >> $filename
 
 SRC=""
 
-for file in *.c ; do
+for file in *.cc ; do
   if [ -f $file ] ; then
     SRC+=" $file"
   fi
 done
 
 echo "SRC=$SRC" >> $filename
-echo "OBJ= \${SRC:.c=.o}" >> $filename
+echo "OBJ= \${SRC:.cc=.o}" >> $filename
+echo "EXEC= $exe" >> $filename
 echo "" >> $filename
 
-echo "all: \${OBJ}" >> $filename
+echo "all: \${EXEC}" >> $filename
+echo "" >> $filename
+
+echo "\${EXEC}: \${OBJ}" >> $filename
+echo -e "\t\${CXX} \${CXXFLAGS} \$^ -o \$@" >> $filename
 echo "" >> $filename
 
 echo "clean:" >> $filename
-echo -e "\t\${RM} \${OBJ}" >> $filename
+echo -e "\t\${RM} \${OBJ} \${EXEC}" >> $filename
 echo "" >> $filename
 
 echo ".PHONY: all clean" >> $filename
+
+echo "Makefile generation complete !"
